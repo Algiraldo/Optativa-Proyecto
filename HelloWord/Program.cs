0a9edd908d4a5ffc9c767a1880414d9010fc0b7a
Algiraldo <alejo.calderon2014@gmail.com>
2025-03-16 17:37:49 -0500

    My second commit

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWord {
+    class Program{
+        static void Main(){
+            string Name="Jhon";
+            Console.WriteLine("Hello World! " + Name);
+            Console.WriteLine();
+        }
+    }
+}
+
+
